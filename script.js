0b2a57c3a88e79b561fe76ef05b275cf3f88c24d
Aditya Srivastava <iamaditya2009@gmail.com>
2021-11-20 20:51:24 +0530

    Arm update

--- script.js
+++ script.js
@@ -57,6 +57,14 @@ class Limb {
   };
   draw = () => this.drawLimb(this.seg);
 
+  getEnd() {
+    var temp = this.seg;
+    while (temp.next) {
+      temp = temp.next;
+    }
+    return { x: temp.x2, y: temp.y2 };
+  }
+
   create(count, endx, endy) {
     if (count == 0) {
       return null;
@@ -93,9 +101,27 @@ class Limb {
   }
 }
 
-var limbs = [];
+function drawLoop(loop) {
+  for (let i = 0; i < loop.length - 1; i++) {
+    ctx.beginPath();
+    ctx.moveTo(loop[i][0], loop[i][1]);
+    ctx.lineTo(loop[i + 1][0], loop[i + 1][1]);
+    ctx.stroke();
+  }
+  if (true) {
+    ctx.beginPath();
+    ctx.moveTo(loop[loop.length - 1][0], loop[loop.length - 1][1]);
+    ctx.lineTo(loop[0][0], loop[0][1]);
+    ctx.stroke();
+  }
+}
 
-var sections = 10;
+var limbs = [];
+limbs.push(
+  new Limb(constants.nsegments, constants.width / 2, constants.height / 2)
+);
+
+var sections = 100;
 var radius = 100;
 for (let i = 0; i < sections; i++) {
   var angle = (1.0 / sections) * 2 * Math.PI * i;
@@ -104,15 +130,41 @@ for (let i = 0; i < sections; i++) {
   limbs.push(new Limb(constants.nsegments, x, y));
 }
 
+var avail = true;
+
 document.addEventListener("mousemove", (e) => {
-  ctx.clearRect(0, 0, constants.width, constants.height);
-  limbs.forEach((limb) => {
-    for (let i = 0; i < 50; i++) {
-      var fixedx = limb.seg.x;
-      var fixedy = limb.seg.y;
-      limb.moveTo(e.clientX, e.clientY);
-      limb.shiftTo(fixedx, fixedy);
+  if (avail) {
+    avail = false;
+    ctx.clearRect(0, 0, constants.width, constants.height);
+    var limbToFollow = limbs[0];
+
+    let fixedx = limbToFollow.seg.x;
+    let fixedy = limbToFollow.seg.y;
+    limbToFollow.moveTo(e.clientX, e.clientY);
+    limbToFollow.shiftTo(fixedx, fixedy);
+
+    let followPoint = limbToFollow.getEnd();
+    limbs.forEach((limb) => {
+      if (limb != limbToFollow) {
+        let fixedx = limb.seg.x;
+        let fixedy = limb.seg.y;
+        limb.moveTo(followPoint.x, followPoint.y);
+        limb.shiftTo(fixedx, fixedy);
+        limb.draw();
+      }
+    });
+
+    var tempSegs = limbs.map((limb) => limb.seg);
+    tempSegs.splice(0, 1);
+    while (tempSegs[0]) {
+      let loop = [];
+      for (let i = 0; i < tempSegs.length; i++) {
+        let temp = tempSegs[i];
+        loop.push([temp.x, temp.y]);
+        tempSegs[i] = tempSegs[i].next;
+      }
+      drawLoop(loop);
     }
-    limb.draw();
-  });
+    avail = true;
+  }
 });
